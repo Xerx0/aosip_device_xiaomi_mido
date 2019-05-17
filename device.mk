bb4bfb44fc580fa52b755606a1613c107df6f032
nato66613 <normandija1945@gmail.com>
2019-05-17 08:50:34 +0000

    mido: Import Binderised PowerHal 1.2 from LA.UM.7.5.r1-04800-8x96.0

--- device.mk
+++ device.mk
@@ -277,12 +277,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    android.hardware.power@1.2-service
 
 # QMI
 PRODUCT_PACKAGES += \
